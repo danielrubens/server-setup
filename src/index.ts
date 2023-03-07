31ed8437a622213ec6c08e4e016d0de8831a98f4
danielrubens <danielrubensdaniel@gmail.com>
2023-03-07 13:41:05 -0300

    error: getaddrinfo EAI_AGAIN db at GetAddrInfoReqWrap.onlookup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from 'express'
+import db from './db'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World')
+  res.send('hello')
 })
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`)
+  console.log(`app runnin on port ${PORT}`)
+  db.runMigrations()
 })
