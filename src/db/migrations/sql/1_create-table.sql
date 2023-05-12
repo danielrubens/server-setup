d5ea5599b9f9edc4f67c97baa37dafbcabd06394
daniel <danielrubensdaniel@gmail.com>
2023-05-12 18:17:36 -0300

    refactor: migration

--- src/db/migrations/sql/1_create-table.sql
+++ src/db/migrations/sql/1_create-table.sql
@@ -1,4 +1,8 @@
-CREATE TABLE user_profile(
-  user_id VARCHAR(64) PRIMARY KEY,
-  name VARCHAR(256)
+CREATE DATABASE perntodo;
+
+CREATE TABLE user_profile (
+  id SERIAL PRIMARY KEY,
+  description VARCHAR(255),
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP
 );
